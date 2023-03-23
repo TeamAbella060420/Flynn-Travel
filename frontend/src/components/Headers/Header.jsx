83c05b29b5242f8956ff1e072af24728e9d73e8c
Muhandis <Muhandis@wf.local>
2023-03-23 18:56:48 +0800

    set up the navlinks of the header which I navigate to each path using an array of object

--- frontend/src/components/Headers/Header.jsx
+++ frontend/src/components/Headers/Header.jsx
@@ -1,10 +1,47 @@
 import React from 'react'
 import '../Headers/header.css'
 import { Container, Row, Button } from 'reactstrap'
+import { Navlink, Link } from 'react-router-dom';
+
+import logo from '../../assets/images/logo.png'
+
+const nav__links = [
+  {
+    path: '/home',
+    display: 'Home',
+  },
+  {
+    path: '#',
+    display: 'About',
+  },
+  {
+    path: '/tours',
+    display: 'Tours',
+  },
+]
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="header">
+      <Container>
+        <Row>
+          <div className="nav__wrapper d-flex align-items-center justify-content-between">
+            {/* ============== logo =================*/}
+              <div className="logo">
+                <img src={logo} alt="" />
+              </div>
+            {/* ============== logo =================*/}
+            {
+              nav__links.map((nav) => (
+                <div>
+                  {nav.display}
+                </div>
+              ))
+            }
+          </div>
+        </Row>
+      </Container>
+    </header>
   )
 }
 

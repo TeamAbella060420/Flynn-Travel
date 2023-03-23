77d9b805d51b232c9f0045d936f8b5090942b059
Muhandis <Muhandis@wf.local>
2023-03-23 16:19:31 +0800

    add package json file in front end folder

--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -1,13 +1,14 @@
 
 import React from 'react'
 import Header from './../Headers/Header'
-import Footer from './../Footers/Footer'
+import Footer from './../Footer/Footer'
+import Routers from '../../router/Routers'
 
 const Layout = () => {
   return (
     <>
       <Header />
-      <div>Layout</div>
+        <Routers />
       <Footer />
     </>
   )
